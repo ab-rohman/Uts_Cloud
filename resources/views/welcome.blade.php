7be790842c6cd6b7817b4b1082ed5606f9b639c8
Abdul_Rohman <rokhman12abdul@gmail.com>
2021-11-09 22:03:49 +0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Abdul UTS Laravel</title>
+    <link rel="stylesheet" href="{{asset('assets/css/styleWelcome.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+</head>
+<body>
+
+    <div class = "h1Container">
+        <h1>
+            <span>w</span>
+            <span>e</span>
+            <span>l</span>
+            <span>c</span>
+            <span>o</span>
+            <span>m</span>
+            <span>e</span>
+        </h1>
+        <h1>
+            <span> </span>
+            <span> </span>
+            <span> </span>
+        </h1>
+        <h1>
+            <span>t</span>
+            <span>o</span>
+            <span> </span>
+            <span>m</span>
+            <span>y</span>
+            <span> </span>
+            <span>l</span>
+            <span>a</span>
+            <span>r</span>
+            <span>a</span>
+            <span>v</span>
+            <span>e</span>
+            <span>l</span>
+        </h1>
+    </div>
+    
+    <div class = "buttonContainer">
+        <a class = "btn btn-dark btnCustom" href = "{{route('home')}}">Start Now</a>
+    </div>
+</body>
+</html>
